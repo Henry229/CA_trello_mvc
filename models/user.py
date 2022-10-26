a1560377650de67b3e55a0bc4b07c0b690ec36b1
Henry Chun <chunhyungjoon@gmail.com>
2022-10-26 12:00:56 +1100

    one to many relation

--- models/user.py
+++ models/user.py
@@ -8,6 +8,8 @@ class User(db.Model):
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
+    # card는 user와 종속관계가 있고 user가 삭제되면 card의 모든 record가 사라짐. 그걸 cascade로 정의
+    cards = db.relationship('Card', back_populates='user', cascade='all, delete')
     
 class UserSchema(ma.Schema):
     class Meta:
